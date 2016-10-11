47d72c40237fb4605f324e80d4b3ba439edef39c
Shaldren <fouillen.dorian@orange.fr>
2016-10-11 17:54:11 +0200

    update header for new member

--- src/application.bat
+++ src/application.bat
@@ -1,10 +1,10 @@
 
-###test_app.bat-2016-BOUDIER###
+###test_app.bat-2016-BOUDIER-Dorian-FOUILLEN###
 echo "Copyright company - 2016"
 echo "Build : 10:00"
 echo "version 1.2.1";
 
-###application.bat-2016-BOUDIER###
+###application.bat-2016-BOUDIER-Dorian-FOUILLEN###
 version="2"
 build_hour=11H00
 
